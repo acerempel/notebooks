5ace752abc06e619535f2ad41622e75cd96f391d
Alan Rempel <alan.rempel@gmail.com>
2020-12-24 16:12:19 -0500

    Fix errors in use of sinuous
    Functions, h, append element

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {DOMParser} from "prosemirror-model"
 import {schema} from "prosemirror-schema-basic"
 import {exampleSetup} from "prosemirror-example-setup"
 
-import { observable, html, subscribe } from 'sinuous';
+import { observable, h } from 'sinuous';
 
 // document.addEventListener("DOMContentLoaded", (event) => ());
 
@@ -21,19 +21,24 @@ function createEditor({ editorId, contentId }) {
 }
 
 const user = observable(null);
+
 function signUp(event) {
   alert("You are signed up!");
   user("Potato");
 }
+
 const Page = props => {
   return html`
+    <header><h1>Good evening</h1></header>
     ${
-      if (user() === null) {
+      () => user() === null ?
         html`
           You are not signed up!
           <button onclick=${signUp}>Sign up</button>
         `
-      } else { html`Hello, ${user()}!` }
+      : html`Hello, ${user}!`
     }
   `;
 }
+
+document.body.append(Page({}));
