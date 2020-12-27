d66dbdf8c0455e628f925dde65dc3a4dbe072e28
Alan Rempel <alan.rempel@gmail.com>
2020-12-27 16:24:45 -0500

    Make it do something
    Namely, fetch a note from the database and display it.

--- src/main.js
+++ src/main.js
@@ -6,39 +6,88 @@ import {DOMParser} from "prosemirror-model"
 import {schema} from "prosemirror-schema-basic"
 import {exampleSetup} from "prosemirror-example-setup"
 
-import { observable, h } from 'sinuous';
+import { observable, subscribe } from 'sinuous/observable';
+import { h } from 'sinuous';
+
+import { createClient } from '@supabase/supabase-js';
 
 // document.addEventListener("DOMContentLoaded", (event) => ());
 
-let view;
-function createEditor({ editorId, contentId }) {
-  view = new EditorView(document.getElementById(editorId), {
-    state: EditorState.create({
-      doc: DOMParser.fromSchema(schema).parse(document.getElementById(contentId)), 
-      plugins: exampleSetup({schema: schema, menuBar: false})
-    })
-  });
-}
-
 const user = observable(null);
 
+const currentNotebook = observable(null);
+const currentNote = observable(null);
+function interpretURL() {
+  // Split on the path separator; ignore the first element, as it is the empty string
+  // (because the path begins with a slash).
+  let pathParts = window.location.pathname.split('/').slice(1);
+  // The first part is the name of the notebook.
+  currentNotebook(pathParts[0]);
+  // The second part is the identifier of the note we are displaying.
+  currentNote(pathParts[1]);
+}
+
 function signUp(event) {
-  alert("You are signed up!");
   user("Potato");
+}
+function signIn(event) {
+  user("Ptomonot");
+}
+function signOut(event) {
+  user(null);
+}
+
+const AuthControls = () => {
+    return user() === null
+      ? html`
+        <button onclick=${signUp}>Create account</button>
+        <button onclick=${signIn}>Sign in</button>
+      ` : html`
+        <strong>${user()}</strong>
+        <button onclick=${signOut}>Sign out</button>
+      `;
+}
+
+function createEditor({ editor, content }) {
+  let state = EditorState.create({
+    doc: DOMParser.fromSchema(schema).parse(content), 
+    plugins: exampleSetup({schema: schema, menuBar: false})
+  });
+  return new EditorView(editor, { state });
 }
 
 const Page = props => {
   return html`
-    <header><h1>Good evening</h1></header>
-    ${
-      () => user() === null ?
-        html`
-          You are not signed up!
-          <button onclick=${signUp}>Sign up</button>
-        `
-      : html`Hello, ${user}!`
-    }
+    <header><h1>Good evening</h1> ${AuthControls}</header>
+    <nav><a onclick=${() => currentNote(1)}>Note 1</a></nav>
+    <main>
+      <article id=editor></article>
+    </main>
   `;
 }
 
 document.body.append(Page({}));
+
+const SUPABASE_URL = "https://qhqomieoafclafetsoxd.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYwODgyMzk3MSwiZXhwIjoxOTI0Mzk5OTcxfQ.ZBHH8NRe8eMVj8xuNUHoLuroL--zvKVO6YYsPS2zJqQ";
+const database = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function fetchNote(noteId) {
+  const { data, error } = await database.from('notes').select().eq('id', noteId);
+  if (error) { console.error(error); }
+  return data;
+}
+
+let editor;
+let noteContent = document.createElement("article");
+subscribe(() => {
+  let noteId = currentNote();
+  if (noteId) {
+    let note = fetchNote(noteId).then(([note]) => {
+      noteContent.innerHTML = note.body;
+      editor = createEditor({ editor: document.getElementById("editor"), content: noteContent });
+    });
+  }
+});
+
+interpretURL();
